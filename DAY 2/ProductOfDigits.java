b828c054cd34f6a96ff38e878e186acecfa073aa
Avanish singh <avanishsinghs@Avanishs-MacBook-Air.local>
2026-06-03 20:39:22 +0530

    first commit

---
+++ DAY 2/ProductOfDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int product = 1;
+
+        while(n != 0) {
+            product = product * (n % 10);
+            n = n / 10;
+        }
+
+        System.out.println(product);
+    }
+}
---
+++ DAY 3/LCM.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int max = (a > b) ? a : b;
+
+        while(true) {
+            if(max % a == 0 && max % b == 0) {
+                System.out.println(max);
+                break;
+            }
+            max++;
+        }
+    }
+}
