33bfe9517afea896e9c95b7ded76d3394f9c301d
Avanish singh <avanishsinghs@Avanishs-MacBook-Air.local>
2026-06-06 20:48:43 +0530

    Added Day 6

---
+++ DAY 6/DecimalToBinary.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println("Binary = 0");
+            return;
+        }
+
+        int[] binary = new int[32];
+        int i = 0;
+
+        while (n > 0) {
+            binary[i] = n % 2;
+            n = n / 2;
+            i++;
+        }
+
+        System.out.print("Binary = ");
+
+        for (int j = i - 1; j >= 0; j--) {
+            System.out.print(binary[j]);
+        }
+
+    
+    }
+}
